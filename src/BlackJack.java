a38b3b5e5ee023bf56fccc013ff177c71b5417cc
CIN33R <badams@bmhs-la.org>
2024-02-27 08:16:24 -0800

    Card class, missing Deck and Blackjack classes

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class BlackJack {
+public class BlackJack {
+
+    public static void main(String[] args) {
+        BlackJack game = new BlackJack();
+        game.run();
+    }
+
+    private void run() {
+        // valid suits are 0-3
+        // valid values are 2-14
+        Card card1 = new Card(0, 2);
+        Card card2 = new Card(0, 3);
+        Card card3 = new Card(0, 4);
+        Card card4 = new Card(0, 5);
+        Card card5 = new Card(0, 6);
+
+        System.out.println(card1);
+        System.out.println(card2);
+        System.out.println(card3);
+        System.out.println(card4);
+        System.out.println(card5);
+
+    }
 }
--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Card {
+public class Card {
+
+    private int suit;
+    private int value;
+
+    public Card(int suit, int value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public int getSuit() {
+        return suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String toString() {
+        String[] suits = {"Hearts", "Clubs", "Diamonds", "Spades"};
+        String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+        return values[this.value-2] + " " + suits[this.suit];
+    }
+
+
+
+
+
+
+
 }
